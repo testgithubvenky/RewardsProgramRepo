9935ee88b728dc663738645361ef30117a03b6b1
testgithubvenky <kondojuvenkatesh1998@gmail.com>
2025-07-15 23:04:04 +0530

    Initial Project Files Commit

---
+++ rewardsprogram/src/main/java/com/customer/rewardsprogram/RewardsprogramApplication.java
@@ -0,0 +1,13 @@
+package com.customer.rewardsprogram;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RewardsprogramApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RewardsprogramApplication.class, args);
+	}
+
+}
